f1d72184eff55ec2895508d06457cb6d7b7cde17
Joey Berger <44282450+joeyBerger@users.noreply.github.com>
2021-09-06 21:41:10 -0700

    added date picker

--- src/client/index.js
+++ src/client/index.js
@@ -1,19 +1,37 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
+import './styles/datepicker.min.scss'
+
+import datepicker from 'js-datepicker'
+
+console.log(datepicker)
+// import './styles/resets.scss'
+// import './styles/base.scss'
+// import './styles/form.scss'
+// import './styles/section.scss'
+
+var today = new Date();
+const tomorrow = {
+    dd : String(today.getDate()),
+    mm : String(today.getMonth()),
+    yyyy : today.getFullYear()
+}
+
+const picker = datepicker(document.querySelector('#calendar'), {minDate: new Date(tomorrow.yyyy, tomorrow.mm, tomorrow.dd)})
+
+console.log(picker)
+
+picker.alwaysShow = true;
+picker.show()
 
 const testButton = document.getElementById('test-button')
 
-
-console.log('wtf')
-
 testButton.addEventListener('click', async e => {
-    e.preventDefault()
-    
+    e.preventDefault()    
     // let res = await fetch('searchPlace/Las Vegas/Nevada/US')
     // let res = await fetch(`searchPlace/${'Houston'}/${'TX'}/${'US'}`)
     // let res = await fetch(`searchPlace/${'Houston'}/${undefined}/${'US'}`)
     let res = await fetch(`searchPlace/${'Tokyo'}/${undefined}/${'Japan'}`)
     res = await res.json()
-
     console.log(res)
 })
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
@@ -14,6 +14,10 @@ module.exports = {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
             }
         ]
     },
@@ -22,14 +26,6 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
-        new CleanWebpackPlugin({
-            // Simulate the removal of files
-            dry: true,
-            // Write Logs to Console
-            verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
-            cleanStaleWebpackAssets: true,
-            protectWebpackAssets: false
-        })
+        new MiniCssExtractPlugin({filename: '[name].css'})
     ]
 }
