fe51b2e1c8660ce572bb12fa2638a5eeb3ffee22
Joey Berger <44282450+joeyBerger@users.noreply.github.com>
2021-09-09 21:51:18 -0700

    added user input forms, expanded api endpoint

--- src/client/index.js
+++ src/client/index.js
@@ -4,7 +4,6 @@ import './styles/datepicker.min.scss'
 
 import datepicker from 'js-datepicker'
 
-console.log(datepicker)
 // import './styles/resets.scss'
 // import './styles/base.scss'
 // import './styles/form.scss'
@@ -26,30 +25,57 @@ let selectedDate;
 const picker = datepicker(document.querySelector('#calendar'), {
     minDate: new Date(tomorrowObj.yyyy, tomorrowObj.mm, tomorrowObj.dd),
     onSelect: (instance, date) => {
-        var tomorrowObj = new Date(date);
-        var B = tomorrowObj.toISOString();
-        selectedDate = B.split('T')[0];
+        if (date) {
+            var tomorrowObj = new Date(date);
+            var B = tomorrowObj.toISOString();
+            selectedDate = B.split('T')[0];
+        } else {selectedDate = ''}
       }
 })
-// console.log(picker)
-
-// const celendarInput = document.getElementById('calendar')
-// celendarInput.addEventListener('click', e => {
-//     e.preventDefault()
-//     console.log(e.target.value)
-// })
 
 picker.alwaysShow = true;
 picker.show()
 
-const testButton = document.getElementById('test-button')
+const findLocation = document.getElementById('find-location')
 
-testButton.addEventListener('click', async e => {
-    e.preventDefault()    
-    // let res = await fetch('searchPlace/Las Vegas/Nevada/US')
-    // let res = await fetch(`searchPlace/${'Houston'}/${'TX'}/${'US'}`)
-    // let res = await fetch(`searchPlace/${'Houston'}/${undefined}/${'US'}`)
-    let res = await fetch(`searchPlace/${'Tokyo'}/${undefined}/${'Japan'}/${selectedDate}`)
+const image = document.getElementById("destination-image");
+
+let inputFields = {
+    city : undefined,
+    state : undefined,
+    country : undefined,
+}
+
+Object.keys(inputFields).forEach(k => inputFields[k] = document.getElementById(k))
+
+
+const handleUserInput = () => {
+    let checksPassed = true;
+    let returnObject = {selectedDate}
+    Object.keys(inputFields).forEach(k => {
+        const {value} = inputFields[k];
+        returnObject[k] = value;
+        if (k !== 'state' && !value) {
+            checksPassed = false;
+        }
+        if (k === 'state' && returnObject[k] === '') returnObject[k] = 'undefined';
+    })
+    if (!selectedDate) checksPassed = false;
+    return !checksPassed ? checksPassed : returnObject;
+}
+
+
+findLocation.addEventListener('click', async e => {
+    e.preventDefault();
+    const userInput = handleUserInput();
+    if (!userInput) return;
+    const {city,state,country,selectedDate} = userInput
+    let res = await fetch(`searchPlace/${city}/${state}/${country}/${selectedDate}`)
     res = await res.json()
+    if (res.error) {
+
+    } else {
+        image.src = res.webformatURL;
+    }
     console.log(res)
 })
--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,12 @@ var path = require('path')
 const express = require('express')
 const axios = require('axios');
 require('dotenv').config({path: __dirname.replace('src/server','') + '/.env'})
-const {getLocationPhoto, formatUserInput, returnDaysBeforeTrip} = require(`${__dirname}/helpers/helperFunctions.js`);
+const {
+    getLocationPhoto,
+    formatUserInput,
+    returnDaysBeforeTrip,
+    convertCelsiusToFarenheight
+} = require(`${__dirname}/helpers/helperFunctions.js`);
 
 const app = express()
 
@@ -22,31 +27,56 @@ app.listen(8081, function () {
 
 app.get('/searchPlace/:city/:state/:country/:travelDate', async (req, res) => {
 
-
     const formattedUserInput = formatUserInput(req.params)
 
-    let response = await axios.get(`http://api.geonames.org/findNearbyPostalCodesJSON?placename=${formattedUserInput}&username=${process.env.GEONAMES_USERNAME}`)
+    let weatherPromise = new Promise(async (resolve,reject) => {    
+        try {
+            let response = await axios.get(`http://api.geonames.org/findNearbyPostalCodesJSON?placename=${formattedUserInput}&username=${process.env.GEONAMES_USERNAME}`)
+            if (!response.data || !response.data.postalCodes || response.data.postalCodes.length === 0) {
+                resolve({error:"Could not find location"})
+                return
+            }
+
+            const {lat,lng} = response.data.postalCodes[0];            
+            const daysBeforeTrip = returnDaysBeforeTrip(req.params)
+            let weatherData = {};
+            if (daysBeforeTrip <= 16) {
+            //for weather in 16 days : https://www.weatherbit.io/api/weather-forecast-16-day
+                response = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lng}&key=${process.env.WEATHERBIT_API_KEY}&include=minutely`)
+                weatherData = response.data.data.find(d => d.valid_date === req.params.travelDate)
+            } else {
+                    //for weather past 16 days : https://www.weatherbit.io/api/weather-history-daily
+                    response = await axios.get(`https://api.weatherbit.io/v2.0/history/daily?lat=${lat}&lon=${lng}&start_date=2021-09-03&end_date=2021-09-04&key=${process.env.WEATHERBIT_API_KEY}&include=minutely`)
+                    weatherData.temp = response.data.data[0].temp
+            }
+            if (!weatherData) resolve({error:"Could not get weather"})
+            else {
+                resolve({
+                    daysBeforeTrip,
+                    celcius : weatherData.temp,
+                    farenheight : convertCelsiusToFarenheight(weatherData.temp)
+                })
+            }
+        } catch(e) {
+            console.log('Error: ', e ? e : 'unkown')
+        }
+    })
+    .catch((e) => console.log('in catch'))
     
-    const {lat,lng} = response.data.postalCodes[0]
+    let imagePromise = new Promise(async (resolve,reject) => {
+        let image = await getLocationPhoto(req.params.city)
+        if (!image) image = await getLocationPhoto(req.params.country)
+        if (!image) image = 'https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png'
+        resolve({webformatURL : image.webformatURL})
+    })    
+    Promise.all([weatherPromise,imagePromise]).then((values) => {
+        console.log(values)
+        if (!values[0].farenheight) {
+            return values[0]
+        }
+        res.send(Object.assign(values[0],values[1]))
+    });
 
-    console.log('travelDate',req.params)
-    const daysBeforeTrip = returnDaysBeforeTrip(req.params)
-    if (daysBeforeTrip <= 16) {
-    //for weather in 16 days : https://api.weatherbit.io/v2.0/forecast/daily?city=Raleigh,NC&key=API_KEY
-        response = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lng}&key=${process.env.WEATHERBIT_API_KEY}&include=minutely`)
-        // console.log(response.data.data)
-        const weatherData = response.data.data.find(d => d.valid_date === req.params.travelDate)
-        console.log('weatherData',weatherData.temp)
-    }
-
-
-
-    //for weather past 16 days : https://api.weatherbit.io/v2.0/history/daily?postal_code=27601&country=US&start_date=2021-09-03&end_date=2021-09-04&key=API_KEY
-    // response = await axios.get(`https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lng}&key=${process.env.WEATHERBIT_API_KEY}&include=minutely`)
-
-
-
-    // console.log(response.data.data[0])
-
-    // let image = await getLocationPhoto(formattedUserInput.replace(',','+'))
+    // console.log(req.params)
+    // res.send({daysBeforeTrip: 2, celcius: 33, farenheight: 91, webformatURL: 'https://pixabay.com/get/g66ee5ec4fe74c8fc409ddf452…dc78a0a01d92ed5ffa1d24eb1d404763a88cc8cd6_640.jpg'}) 
 })
