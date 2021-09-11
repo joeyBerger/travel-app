229a4b097cfc6b117107c22f00629949f80b4384
Joey Berger <44282450+joeyBerger@users.noreply.github.com>
2021-09-11 10:04:29 -0700

    saving and deleting trips, better calendar css, custom css

--- src/client/index.js
+++ src/client/index.js
@@ -1,14 +1,15 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
-import './styles/datepicker.min.scss'
+
 
 import datepicker from 'js-datepicker'
 
-// import './styles/resets.scss'
-// import './styles/base.scss'
-// import './styles/form.scss'
-// import './styles/section.scss'
-
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/form.scss'
+import './styles/section.scss'
+import './styles/datepicker.min.scss'
+import './styles/custom-styles.scss'
 
 const today = new Date()
 const tomorrow = new Date(today)
@@ -30,15 +31,22 @@ const picker = datepicker(document.querySelector('#calendar'), {
             var B = tomorrowObj.toISOString();
             selectedDate = B.split('T')[0];
         } else {selectedDate = ''}
-      }
+    },
+    customDays: ['S', 'M', 'T', 'W', 'T', 'F', 'S']
 })
+picker.calendarContainer.style.setProperty('font-size', '1.5rem')
+
+console.log(picker)
 
 picker.alwaysShow = true;
 picker.show()
 
-const findLocation = document.getElementById('find-location')
+const savedTrips = [];
 
+const inputForm = document.getElementById('input-form')
+const findLocation = document.getElementById('find-location')
 const image = document.getElementById("destination-image");
+const inputErrorMessages = document.getElementsByClassName("input-error");
 
 let inputFields = {
     city : undefined,
@@ -48,6 +56,10 @@ let inputFields = {
 
 Object.keys(inputFields).forEach(k => inputFields[k] = document.getElementById(k))
 
+// let width = document.body.clientHeight
+// window.addEventListener('resize',() => {
+//     console.log(document.body.clientWidth)
+// })
 
 const handleUserInput = () => {
     let checksPassed = true;
@@ -55,19 +67,44 @@ const handleUserInput = () => {
     Object.keys(inputFields).forEach(k => {
         const {value} = inputFields[k];
         returnObject[k] = value;
+        console.log(value)
         if (k !== 'state' && !value) {
             checksPassed = false;
+            for (let i = 0; i < inputErrorMessages.length; i++) {
+                if (inputErrorMessages[i].id.includes(k)) inputErrorMessages[i].style.display = 'block'
+            }
         }
         if (k === 'state' && returnObject[k] === '') returnObject[k] = 'undefined';
     })
-    if (!selectedDate) checksPassed = false;
+    if (!selectedDate) {
+        checksPassed = false;
+        for (let i = 0; i < inputErrorMessages.length; i++) {
+            if (inputErrorMessages[i].id.includes('calendar-error')) inputErrorMessages[i].style.display = 'block'
+        }
+    }
     return !checksPassed ? checksPassed : returnObject;
 }
 
+const createCustomElement = (tag,id,className,innerHTML,src,event) => {
+    const element = document.createElement(tag);
+    if (id) element.setAttribute("id", id);
+    if (className) element.setAttribute("class", className);
+    if (innerHTML) element.innerHTML = innerHTML;
+    if (src) {
+        element.src = 'https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Flaurabegleybloom%2Ffiles%2F2018%2F06%2FCappadocia-by-%40izkiz-1200x801.jpg';
+        console.log(src)
+    }
+    if (event) element.addEventListener(event.type,event.action)
+    
+    return element;
+}
 
 findLocation.addEventListener('click', async e => {
     e.preventDefault();
+    for (let i = 0; i < inputErrorMessages.length; i++) inputErrorMessages[i].style.display = 'none'
     const userInput = handleUserInput();
+    console.log(userInput)
+
     if (!userInput) return;
     const {city,state,country,selectedDate} = userInput
     let res = await fetch(`searchPlace/${city}/${state}/${country}/${selectedDate}`)
@@ -75,7 +112,60 @@ findLocation.addEventListener('click', async e => {
     if (res.error) {
 
     } else {
-        image.src = res.webformatURL;
+        inputForm.style.display = 'none'
+        // image.src = res.webformatURL;
+        // const destination = document.getElementById('destination')
+        // destination.innerHTML = `${city}${state!=='undefined'?`, ${state}`:''} ${country}`
+        // const travelDate = document.getElementById('travel-date')
+        // travelDate.innerHTML = `Days Until Trip: ${res.daysBeforeTrip}`;
+        // const forecast = document.getElementById('forecast');
+        // forecast.innerHTML = `Forecast: ${res.celcius}° / ${res.farenheight}°`;
+
+        const tripId = savedTrips.length;
+        const plannedTrips = document.getElementById('planned-trips');
+        const container = createCustomElement('section',`trip-${tripId}`)
+
+        savedTrips.forEach(trip => trip.style.display = 'none')
+
+        let elements = [];
+        elements.push(createCustomElement('h3','destination',undefined,`${city}${state!=='undefined'?`, ${state}`:''} ${country}`));
+        elements.push(createCustomElement('h4','forecast',undefined,`Forecast: ${res.celcius}° C / ${res.farenheight}° F`));
+        elements.push(createCustomElement('h4','travel-date',undefined,`Days Until Trip: ${res.daysBeforeTrip}`));
+        // elements.push(createCustomElement('img','destination-image',undefined,undefined,res.webformatURL));
+
+        elements.push(createCustomElement('div',undefined,'image-container',undefined,undefined));
+        let image = createCustomElement('img','destination-image',undefined,undefined,res.webformatURL);
+        elements[elements.length-1].appendChild(image)
+
+
+        // elements.push(createCustomElement('div',undefined,'image-container',undefined,undefined));
+        // // let image = createCustomElement('img','destination-image',undefined,undefined,res.webformatURL);
+        // let image = createCustomElement('div','destination-image',undefined,'TEST');
+        // elements[elements.length-1].appendChild(image)
+
+
+        elements.push(createCustomElement('div',undefined,'button-container',undefined,undefined));
+
+        let button = createCustomElement('button','save-button',undefined,'Save',undefined,{type : 'click', action : () => {
+            console.log('Save',tripId)            
+            inputForm.style.display = 'block'
+            savedTrips.forEach(trip => trip.style.display = 'block')
+            document.getElementById('save-button').remove()
+            document.getElementById(`cancel-button-${tripId}`).innerHTML = 'Remove Trip'            
+        }});
+        elements[elements.length-1].appendChild(button)
+
+        button = createCustomElement('button',`cancel-button-${tripId}`,undefined,'Cancel',undefined,{type : 'click', action : () => {
+            inputForm.style.display = 'block'
+            container.remove();
+            savedTrips.forEach(trip => trip.style.display = 'block')
+            savedTrips.slice(tripId,tripId)
+        }});
+        elements[elements.length-1].appendChild(button)
+
+        elements.forEach(e => container.appendChild(e))
+        plannedTrips.appendChild(container)
+        savedTrips.push(container)
     }
     console.log(res)
 })
--- src/server/index.js
+++ src/server/index.js
@@ -20,6 +20,8 @@ app.get('/', function (req, res) {
     res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
+let tempNumber = 0;
+
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
     console.log('Example app listening on port 8081!')
@@ -29,54 +31,55 @@ app.get('/searchPlace/:city/:state/:country/:travelDate', async (req, res) => {
 
     const formattedUserInput = formatUserInput(req.params)
 
-    let weatherPromise = new Promise(async (resolve,reject) => {    
-        try {
-            let response = await axios.get(`http://api.geonames.org/findNearbyPostalCodesJSON?placename=${formattedUserInput}&username=${process.env.GEONAMES_USERNAME}`)
-            if (!response.data || !response.data.postalCodes || response.data.postalCodes.length === 0) {
-                resolve({error:"Could not find location"})
-                return
-            }
+    // let weatherPromise = new Promise(async (resolve,reject) => {    
+    //     try {
+    //         let response = await axios.get(`http://api.geonames.org/findNearbyPostalCodesJSON?placename=${formattedUserInput}&username=${process.env.GEONAMES_USERNAME}`)
+    //         if (!response.data || !response.data.postalCodes || response.data.postalCodes.length === 0) {
+    //             resolve({error:"Could not find location"})
+    //             return
+    //         }
 
-            const {lat,lng} = response.data.postalCodes[0];            
-            const daysBeforeTrip = returnDaysBeforeTrip(req.params)
-            let weatherData = {};
-            if (daysBeforeTrip <= 16) {
-            //for weather in 16 days : https://www.weatherbit.io/api/weather-forecast-16-day
-                response = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lng}&key=${process.env.WEATHERBIT_API_KEY}&include=minutely`)
-                weatherData = response.data.data.find(d => d.valid_date === req.params.travelDate)
-            } else {
-                    //for weather past 16 days : https://www.weatherbit.io/api/weather-history-daily
-                    response = await axios.get(`https://api.weatherbit.io/v2.0/history/daily?lat=${lat}&lon=${lng}&start_date=2021-09-03&end_date=2021-09-04&key=${process.env.WEATHERBIT_API_KEY}&include=minutely`)
-                    weatherData.temp = response.data.data[0].temp
-            }
-            if (!weatherData) resolve({error:"Could not get weather"})
-            else {
-                resolve({
-                    daysBeforeTrip,
-                    celcius : weatherData.temp,
-                    farenheight : convertCelsiusToFarenheight(weatherData.temp)
-                })
-            }
-        } catch(e) {
-            console.log('Error: ', e ? e : 'unkown')
-        }
-    })
-    .catch((e) => console.log('in catch'))
+    //         const {lat,lng} = response.data.postalCodes[0];            
+    //         const daysBeforeTrip = returnDaysBeforeTrip(req.params)
+    //         let weatherData = {};
+    //         if (daysBeforeTrip <= 16) {
+    //         //for weather in 16 days : https://www.weatherbit.io/api/weather-forecast-16-day
+    //             response = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lng}&key=${process.env.WEATHERBIT_API_KEY}&include=minutely`)
+    //             weatherData = response.data.data.find(d => d.valid_date === req.params.travelDate)
+    //         } else {
+    //                 //for weather past 16 days : https://www.weatherbit.io/api/weather-history-daily
+    //                 response = await axios.get(`https://api.weatherbit.io/v2.0/history/daily?lat=${lat}&lon=${lng}&start_date=2021-09-03&end_date=2021-09-04&key=${process.env.WEATHERBIT_API_KEY}&include=minutely`)
+    //                 weatherData.temp = response.data.data[0].temp
+    //         }
+    //         if (!weatherData) resolve({error:"Could not get weather"})
+    //         else {
+    //             resolve({
+    //                 daysBeforeTrip,
+    //                 celcius : weatherData.temp,
+    //                 farenheight : convertCelsiusToFarenheight(weatherData.temp)
+    //             })
+    //         }
+    //     } catch(e) {
+    //         console.log('Error: ', e ? e : 'unkown')
+    //     }
+    // })
+    // .catch((e) => console.log('in catch'))
     
-    let imagePromise = new Promise(async (resolve,reject) => {
-        let image = await getLocationPhoto(req.params.city)
-        if (!image) image = await getLocationPhoto(req.params.country)
-        if (!image) image = 'https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png'
-        resolve({webformatURL : image.webformatURL})
-    })    
-    Promise.all([weatherPromise,imagePromise]).then((values) => {
-        console.log(values)
-        if (!values[0].farenheight) {
-            return values[0]
-        }
-        res.send(Object.assign(values[0],values[1]))
-    });
+    // let imagePromise = new Promise(async (resolve,reject) => {
+    //     let image = await getLocationPhoto(req.params.city)
+    //     if (!image) image = await getLocationPhoto(req.params.country)
+    //     if (!image) image = 'https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png'
+    //     resolve({webformatURL : image.webformatURL})
+    // })    
+    // Promise.all([weatherPromise,imagePromise]).then((values) => {
+    //     console.log(values)
+    //     if (!values[0].farenheight) {
+    //         return values[0]
+    //     }
+    //     res.send(Object.assign(values[0],values[1]))
+    // });
+    
 
     // console.log(req.params)
-    // res.send({daysBeforeTrip: 2, celcius: 33, farenheight: 91, webformatURL: 'https://pixabay.com/get/g66ee5ec4fe74c8fc409ddf452…dc78a0a01d92ed5ffa1d24eb1d404763a88cc8cd6_640.jpg'}) 
+    res.send({daysBeforeTrip: tempNumber++, celcius: 33, farenheight: 91, webformatURL: 'https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Flaurabegleybloom%2Ffiles%2F2018%2F06%2FCappadocia-by-%40izkiz-1200x801.jpg'}) 
 })
